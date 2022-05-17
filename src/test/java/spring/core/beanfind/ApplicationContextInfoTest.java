472fb2ae9247237a1b521d051a7481ea73ea618c
SONGYOUNGWOO <jeroso@ldfactory.com>
2022-05-17 13:34:46 +0900

    빈 출력 테스트케이스 작성

--- src/test/java/spring/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/spring/core/beanfind/ApplicationContextInfoTest.java
@@ -2,17 +2,37 @@ package spring.core.beanfind;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AppConfig;
 
 public class ApplicationContextInfoTest {
 
-    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
-        ac.getBeanDefinitionNames();
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+
+    }
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION : 애플리케이션을 개발하기 위해서 직접 등록한 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+
     }
 }
