d070eb45cd552e0547b78e03d1ebda1ccf24e71c
SONGYOUNGWOO <jeroso@ldfactory.com>
2022-05-17 17:48:06 +0900

    같은 타입의 빈 중복 테스트 ing

---
+++ src/test/java/spring/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,43 @@
+package spring.core.beanfind;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.core.AppConfig;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemoryMemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(sameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시에 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다")
+    void findBeanByTypeDuplicate() {
+//        MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class)
+        );
+    }
+
+    @Configuration
+    static class sameBeanConfig {
+
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+
+}
