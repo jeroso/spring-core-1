7faff153b26b1619b10215ac823f57a61fbc6dde
jeroso <jeroso@nate.com>
2022-05-18 22:02:39 +0900

    싱글톤 방식의 문제점 테스트

--- src/test/java/spring/core/singleton/StatefulService.java
+++ src/test/java/spring/core/singleton/StatefulService.java
@@ -2,15 +2,19 @@ package spring.core.singleton;
 
 public class StatefulService {
 
-    private int price;  //상태를 유지하는 필드
+//    스프링 빈은 항상 stateless(무상태)를 설계해야 한다.
 
-    public void order(String name, int price) {
+//    private int price;  //상태를 유지하는 필드
+
+//    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name + \" price = \" + price = " + name + " price = " + price);
-        this.price = price; // 여기가 문제!
-    }
-
-    public int getPrice() {
+//        this.price = price; // 여기가 문제!
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 }
