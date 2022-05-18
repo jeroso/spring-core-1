6cd40a14079d8ea1d24b5f6a1c3d0f3e5707d3f9
woodsy <jeroso4481@gmail.com>
2022-05-18 23:42:28 +0900

    @Configuration 과 바이트코드 조작의 마법

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,5 +1,6 @@
 package spring.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.core.discount.DiscountPolicy;
@@ -15,10 +16,13 @@ import spring.core.order.OrderServiceImpl;
 @Configuration
 public class AppConfig {
 
+//    @Autowired MemberRepository memberRepository;
+
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
+//        return new MemberServiceImpl(memberRepository); @Autowired 를 사용해서 주입해주는 방식도 있음
     }
 
     @Bean
@@ -31,6 +35,7 @@ public class AppConfig {
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
+//        return new OrderServiceImpl(memberRepository, discountPolicy()); @Autowired 사용
     }
 
     @Bean
