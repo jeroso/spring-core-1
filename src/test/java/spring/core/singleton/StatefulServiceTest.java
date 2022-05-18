8ea817e7424bb5415c2637eca0ba08a7ae7abfd2
jeroso <jeroso@nate.com>
2022-05-18 23:17:00 +0900

    @Configuration과 싱글톤

--- src/test/java/spring/core/singleton/StatefulServiceTest.java
+++ src/test/java/spring/core/singleton/StatefulServiceTest.java
@@ -19,15 +19,15 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A 사용자가 10000원 주문
-        statefulService1.order("userA", 10000);
+        int price1 = statefulService1.order("userA", 10000);
         //ThreadB: B 사용자가 20000원 주문
-        statefulService2.order("userB", 20000);
+        int price2 = statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자 A 주문금액 조회회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + price1);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        assertThat(price2).isEqualTo(20000);
     }
     static class TestConfig{
 
