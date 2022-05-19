88477aaab7bff910be44227f0f299b7cd540fc56
jeroso <jeroso@nate.com>
2022-05-19 23:46:12 +0900

    @ComponentScan 테스트

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -1,4 +1,4 @@
-package spring.core;
+package spring.core;    // basePackages 가 없을 시 해당 패키지 하위를 모두 탐색 한다.
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "spring.core.member",    //member만 @componentScan 대상이 됨
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)  //AppConfig.class 를 제외한다.
 )
 public class AutoAppConfig {
-
 
 }
