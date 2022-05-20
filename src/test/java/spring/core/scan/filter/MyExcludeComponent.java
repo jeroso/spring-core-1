e678a80babf1a39cf0bd3aa2a37d1cab23ca8e4b
SONGYOUNGWOO <jeroso@ldfactory.com>
2022-05-20 10:45:08 +0900

    컴포넌트 스캔 필터

---
+++ src/test/java/spring/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package spring.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)   //class 레벨에 붙는다.
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
