1566012fae4000efbd09b27c1efb8bf805c63756
jeroso <jeroso@nate.com>
2022-05-22 17:44:33 +0900

    생성자 주입을 선택하라!

---
+++ src/test/java/spring/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package spring.core.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.Grade;
+import spring.core.member.Member;
+import spring.core.member.MemoryMemberRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        memoryMemberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(10000);
+    }
+
+}
