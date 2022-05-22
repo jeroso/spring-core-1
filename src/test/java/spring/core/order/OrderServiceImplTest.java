50550a66eb583e447fc18eeffa6a3561628cf384
jeroso <jeroso@nate.com>
2022-05-22 23:14:16 +0900

    조회한 빈이 모두 필요할 때, List, Map

--- src/test/java/spring/core/order/OrderServiceImplTest.java
+++ src/test/java/spring/core/order/OrderServiceImplTest.java
@@ -17,7 +17,7 @@ class OrderServiceImplTest {
         memoryMemberRepository.save(new Member(1L, "name", Grade.VIP));
         OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
         Order order = orderService.createOrder(1L, "itemA", 10000);
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
 }
